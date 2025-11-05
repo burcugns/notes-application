ac202d6f744af2bda93b726c8032e68cc869ebc5
Burcu Gunes <burcudagli.10@gmail.com>
2025-11-05 23:23:03 +0000

    initial APIs added

---
+++ server.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+
+
+const app = express();
+const PORT = 3001;
+app.use(express.json());
+
+
+const dataFilePath = path.join(__dirname, "data.json");
+
+
+const readData = () => {
+  if (!fs.existsSync(dataFilePath)) {
+    return [];
+  }
+  const data = fs.readFileSync(dataFilePath);
+  return JSON.parse(data);
+};
+
+
+const writeData = (data) => {
+  fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
+};
+
+
+
+//get all data
+app.get("/note/getAll", (req,res) => {
+  const data = readData();
+  res.json(data);
+});
+
+//get data by id
+app.get("/note/:id", (req, res) => {
+  const id = req.params;
+  const note = readData().find((note) => note.id === parseInt(id))
+  if (note) {
+    res.status(200).json(note);
+  } else {
+    res.status(404).send("Note not found");
+  }
+});
+
+
+//post
+app.post("/note", (req, res) => {
+  const newData = { id: uuidv4(), ...req.body };
+  const data = readData();
+  data.push(newData);
+  writeData(data);
+  res.json({ message: "Data saved successfully", data: newData });
+});
+
+
+// update
+app.put("/note", (req, res) => {
+  const id = req.body['id'];
+  const data = readData();  
+  const newData =[];
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id === id) {
+      newData.push(req.body);
+    }else{
+      newData.push(data[i]);
+    }
+  }
+
+  writeData(newData);
+});
+
+//delete
+app.delete("/note/:id", (req, res) => {
+  const {id} = req.params;
+  const data = readData();
+  const newData=[];
+  for(let i=0; i<data.length; i++){
+    if(data[i].id!==id){
+      newData.push(data[i]);
+    }
+  }
+  writeData(newData);
+});
+
+//start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
